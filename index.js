4ed97e4ef7c0e6d5648b8676d19c2b3b9e9780ac
Hala AlArid <halaalarid99@gmail.com>
2022-10-24 00:26:09 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(arr, string){
+    const thanksArr = [];
+    for(let i = 0; i < arr.length; i++){
+        thanksArr.push("Thank you, "+ arr[i] + ", for the wonderful "+ string + " gift!")
+    }
+    return thanksArr;
+}
+
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+function countDown(n){
+
+var countdown = n;
+while (countdown >= 0){
+    console.log(countdown);
+    countdown--;
+}
+}
+
+countDown(10);
+
